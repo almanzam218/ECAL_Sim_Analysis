0b06fb24103992d165325110d994e76d7a1408f0
Melissa Almanza Soto <almanzam@glui01.ific.uv.es>
2024-08-23 18:35:13 +0200

    works

--- GetEnergyResolution/scripts/runPixelizationProcessor.sh
+++ GetEnergyResolution/scripts/runPixelizationProcessor.sh
@@ -6,11 +6,14 @@ source ../../init_ilcsoft.sh
 cd /lhome/ific/a/almanzam/testsSim/ExampleProcessor/GetEnergyResolution/scripts
 export MARLIN_DLL="$MARLIN_DLL:$PWD/../lib/libGetEnergyResolutionProcessor.so"
 
-declare -i numberEvents=3
-AIDAFilePath="/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/"
-AIDAFileName="testECALe_luxe_v0_QGSP_BERT_e-_"
+declare -i numberEvents=4
 
 for energy in 1500 
 do
-    Marlin --global.MaxRecordNumber=${numberEvents} --MyAIDAProcessor.FileName="$AIDAFilePath$AIDAFileName$energy" --global.LCIOInputFiles=/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/PixelizationLCIO/new_ECALe_luxe_v0_QGSP_BERT_e-_1500MeV_0.slcio test.xml
+    AIDAFilePath="/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/"
+    AIDAFileName="test1ECALe_luxe_v0_QGSP_BERT_e-_${energy}"
+
+    InputLCIOFilePath="/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/PixelizationLCIO/"
+    InputLCIOFileName="new_ECALe_luxe_v0_QGSP_BERT_e-_${energy}MeV_0.slcio"
+    Marlin --global.MaxRecordNumber=${numberEvents} --MyAIDAProcessor.FileName="$AIDAFilePath$AIDAFileName" --global.LCIOInputFiles=$InputLCIOFilePath$InputLCIOFileName test.xml
 done
