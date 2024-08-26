9ab2cde18cf1d6a5ba51c9bd1bfbd53f2aa2061f
Melissa Almanza Soto <almanzam@glui01.ific.uv.es>
2024-08-26 16:09:42 +0200

    Trying to add fit to energy resolution

--- GetEnergyResolution/PlotEnergyResolution.cc
+++ GetEnergyResolution/PlotEnergyResolution.cc
@@ -11,7 +11,7 @@
 
 
 double GetFitParams(int beamEnergyMeV){
-    TFile f(Form("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/ECALe_luxe_v0_QGSP_BERT_e-_%dMeV.root",beamEnergyMeV));
+    TFile f(Form("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/ECALe_luxe_v0_QGSP_BERT_e-_%dMeV_0.root",beamEnergyMeV));
     TH1F *energyHist = (TH1F*)f.Get("MyGetEnergyResolutionProcessor/_evEnergyHist");
     energyHist->Fit("gaus");
     TF1 *fit = energyHist->GetFunction("gaus");
@@ -27,22 +27,29 @@ void PlotEnergyResolution(){
     TCanvas *c1 = new TCanvas("c1","Energy Resolution",1920,0,1920,1000);
     
 
-    double Resolution[4];
-    double beamEnergy[4];
+    double Resolution[7];
+    double beamEnergy[7];
 
 
     int h=0;
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 7; i++)
     {
-        Resolution[h] = GetFitParams(1500+500*i);
-        beamEnergy[h] = (1500+500*i);// in GeV
+        Resolution[h] = GetFitParams(1500+2000*i);
+        beamEnergy[h] = (1500+2000*i);// in GeV
         h++;
     }
-    auto gEnergyRes = new TGraph(4,beamEnergy,Resolution);
+    auto gEnergyRes = new TGraph(7,beamEnergy,Resolution);
+    gEnergyRes->SetTitle("Energy Resolution");
+    gEnergyRes->GetXaxis()->SetTitle("Beam Energy (MeV)");
+    gEnergyRes->GetYaxis()->SetTitle("#frac{#sigma}{E}");
+    
 
     c1->cd();
     gEnergyRes->Draw("AC*");
-    c1->Print("testPlot.png");
+    TF1 *f1 = new TF1("f1","sqrt([0]/x+[1]/(x*x)+[2])",1500,13500);
+    gEnergyRes->Fit(f1);
+
+    c1->Print("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/EnergyResolutionPlot_20240821.png");
 
 
     return;
--- GetEnergyResolution/scripts/runGetEnergyResolutionProcessor.sh
+++ GetEnergyResolution/scripts/runGetEnergyResolutionProcessor.sh
@@ -6,12 +6,12 @@ source ../../init_ilcsoft.sh
 cd /lhome/ific/a/almanzam/testsSim/ExampleProcessor/GetEnergyResolution/scripts
 export MARLIN_DLL="$MARLIN_DLL:$PWD/../lib/libGetEnergyResolutionProcessor.so"
 
-declare -i numberEvents=4
+declare -i numberEvents=10001
 
-for energy in 1500 2000 
+for energy in 1500 3500 5500 7500 9500 11500 13500 15000
 do
     AIDAFilePath="/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/"
-    AIDAFileName="test1ECALe_luxe_v0_QGSP_BERT_e-_${energy}"
+    AIDAFileName="ECALe_luxe_v0_QGSP_BERT_e-_${energy}MeV_0"
 
     InputLCIOFilePath="/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/PixelizationLCIO/"
     InputLCIOFileName="new_ECALe_luxe_v0_QGSP_BERT_e-_${energy}MeV_0.slcio"
