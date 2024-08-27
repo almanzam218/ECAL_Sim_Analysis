b2771c743fdc4ef1083d6aa2cf7ab00b87ae8e9d
Melissa Almanza Soto <almanzam@glui01.ific.uv.es>
2024-08-27 16:10:26 +0200

    works

--- GetEnergyResolution/PlotEnergyResolution.cc
+++ GetEnergyResolution/PlotEnergyResolution.cc
@@ -11,11 +11,11 @@
 
 
 double GetFitParamsHits(int beamEnergyMeV,int param){
-    TFile f(Form("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/ECALe_luxe_v0_QGSP_BERT_e-_%dMeV_0.root",beamEnergyMeV));
+    TFile f(Form("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/20240821_pixelised/ECALe_luxe_v0_QGSP_BERT_e-_%dMeV_0.root",beamEnergyMeV));
     TH1F *hitsHist = (TH1F*)f.Get("MyGetEnergyResolutionProcessor/_evHitsHist");
     hitsHist->Fit("gaus");
     TF1 *fit = hitsHist->GetFunction("gaus");
-    hitsHist->Fit("gaus","","",fit->GetParameter(1)-fit->GetParameter(2)*2,fit->GetParameter(1)+fit->GetParameter(2)*2);
+    hitsHist->Fit("gaus","","",fit->GetParameter(1)-fit->GetParameter(2)*1,fit->GetParameter(1)+fit->GetParameter(2)*1);
     TF1 *fit1 = hitsHist->GetFunction("gaus");
 
     double fitParams[3] = {fit1->GetParameter(0),fit1->GetParameter(1),fit1->GetParameter(2)};
@@ -24,11 +24,11 @@ double GetFitParamsHits(int beamEnergyMeV,int param){
     return fitParams[param];
 }
 double GetFitParamsEnergy(int beamEnergyMeV,int param){
-    TFile f(Form("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/ECALe_luxe_v0_QGSP_BERT_e-_%dMeV_0.root",beamEnergyMeV));
+    TFile f(Form("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/20240821_pixelised/ECALe_luxe_v0_QGSP_BERT_e-_%dMeV_0.root",beamEnergyMeV));
     TH1F *energyHist = (TH1F*)f.Get("MyGetEnergyResolutionProcessor/_evEnergyHist");
     energyHist->Fit("gaus");
     TF1 *fit = energyHist->GetFunction("gaus");
-    energyHist->Fit("gaus","","",fit->GetParameter(1)-fit->GetParameter(2)*2,fit->GetParameter(1)+fit->GetParameter(2)*2);
+    energyHist->Fit("gaus","","",fit->GetParameter(1)-fit->GetParameter(2)*1,fit->GetParameter(1)+fit->GetParameter(2)*1);
     TF1 *fit1 = energyHist->GetFunction("gaus");
 
     double fitParams[3] = {fit1->GetParameter(0),fit1->GetParameter(1),fit1->GetParameter(2)};
@@ -45,78 +45,79 @@ void PlotEnergyResolution(){
     TCanvas *c4 = new TCanvas("c4","Hit Linearity",1920,0,1920,1000);
     
 
-    double EnergyResolution[7];
-    double meanEnergy[7];
-    double HitResolution[7];
-    double meanHits[7];
-    double beamEnergy[7];
+    double EnergyResolution[28];
+    double meanEnergy[28];
+    double HitResolution[28];
+    double meanHits[28];
+    double beamEnergy[28];
 
 
     int h=0;
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 28; i++)
     {
-        EnergyResolution[h] = GetFitParamsEnergy(1500+2000*i,2)/GetFitParamsEnergy(1500+2000*i,1);
-        meanHits[h] = GetFitParamsHits(1500+2000*i,1);
-        HitResolution[h] = GetFitParamsHits(1500+2000*i,2)/GetFitParamsHits(1500+2000*i,1);
-        meanEnergy[h] = GetFitParamsEnergy(1500+2000*i,1);
-        beamEnergy[h] = (1500+2000*i)/1000;// in GeV
+        EnergyResolution[h] = GetFitParamsEnergy(1500+500*i,2)/GetFitParamsEnergy(1500+500*i,1);
+        meanHits[h] = GetFitParamsHits(1500+500*i,1);
+        HitResolution[h] = GetFitParamsHits(1500+500*i,2)/GetFitParamsHits(1500+500*i,1);
+        meanEnergy[h] = GetFitParamsEnergy(1500+500*i,1);
+        beamEnergy[h] = (1.500+0.500*i);// in GeV
         h++;
     }
-    auto gEnergyRes = new TGraph(7,beamEnergy,EnergyResolution);
+    auto gEnergyRes = new TGraph(28,beamEnergy,EnergyResolution);
     gEnergyRes->SetTitle("Energy Resolution");
-    gEnergyRes->GetXaxis()->SetTitle("Beam Energy (MeV)");
+    gEnergyRes->GetXaxis()->SetTitle("Beam Energy (GeV)");
     gEnergyRes->GetYaxis()->SetTitle("#frac{#sigma}{E}");
     
-    auto gEnergyLin = new TGraph(7,beamEnergy,meanEnergy);
+    auto gEnergyLin = new TGraph(28,beamEnergy,meanEnergy);
     gEnergyLin->SetTitle("Energy Linearity");
-    gEnergyLin->GetXaxis()->SetTitle("Beam Energy (MeV)");
+    gEnergyLin->GetXaxis()->SetTitle("Beam Energy (GeV)");
     gEnergyLin->GetYaxis()->SetTitle("mean E");
     
-    auto gHitRes = new TGraph(7,beamEnergy,HitResolution);
+    auto gHitRes = new TGraph(28,beamEnergy,HitResolution);
     gHitRes->SetTitle("Hit Resolution");
-    gHitRes->GetXaxis()->SetTitle("Beam Energy (MeV)");
+    gHitRes->GetXaxis()->SetTitle("Beam Energy (GeV)");
     gHitRes->GetYaxis()->SetTitle("#frac{#sigma}{E}");
     
-    auto gHitLin = new TGraph(7,beamEnergy,meanHits);
+    auto gHitLin = new TGraph(28,beamEnergy,meanHits);
     gHitLin->SetTitle("Hit Linearity");
-    gHitLin->GetXaxis()->SetTitle("Beam Energy (MeV)");
+    gStyle->SetStatH(0.1);
+    gHitLin->GetXaxis()->SetTitle("Beam Energy (GeV)");
     gHitLin->GetYaxis()->SetTitle("mean hits");
 
     c1->cd();
-    gEnergyRes->Draw("AC*");
+    gEnergyRes->Draw("A*");
     TF1 *f1 = new TF1("f1","sqrt(([0]^2/x)+([1]/x)^2+[2]^2)");
     f1->SetParameters(0,0,0);
     f1->SetParNames ("a","b","c");
-    gEnergyRes->Fit(f1,"","",1,13);
+    gEnergyRes->Fit(f1,"","",1.500,15.000);
     gStyle->SetOptFit(1);
     
     c2->cd();
-    gEnergyLin->Draw("AC*");
+    gEnergyLin->Draw("A*");
     TF1 *f2 = new TF1("f2","[0]*x^[1]");
     f2->SetParameters(0,0);
-    gEnergyLin->Fit(f2,"","",1,13);
+    gEnergyLin->Fit(f2,"","",1.500,15.000);
     gStyle->SetOptFit(1);
 
-    c1->Print("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/EnergyResolutionPlot_20240821.png");
-    c2->Print("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/EnergyLinearityPlot_20240821.png");
+    c1->Print("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/20240821_pixelised/EnergyResolutionPlot_20240821.png");
+    c2->Print("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/20240821_pixelised/EnergyLinearityPlot_20240821.png");
 
     c3->cd();
-    gHitRes->Draw("AC*");
+    gHitRes->Draw("A*");
     TF1 *f3 = new TF1("f3","sqrt(([0]^2/x)+([1]/x)^2+[2]^2)");
     f3->SetParameters(0,0,0);
     f3->SetParNames ("a","b","c");
-    gHitRes->Fit(f3,"","",1,13);
+    gHitRes->Fit(f3,"","",1.500,15.000);
     gStyle->SetOptFit(1);
     
     c4->cd();
-    gHitLin->Draw("AC*");
+    gHitLin->Draw("A*");
     TF1 *f4 = new TF1("f4","[0]*x^[1]");
     f4->SetParameters(10,-10);
-    gHitLin->Fit(f4,"","",1,13);
+    gHitLin->Fit(f4,"","",1.500,15.000);
     gStyle->SetOptFit(1);
 
-    c3->Print("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/HitResolutionPlot_20240821.png");
-    c4->Print("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/HitLinearityPlot_20240821.png");
+    c3->Print("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/20240821_pixelised/HitResolutionPlot_20240821.png");
+    c4->Print("/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/20240821_pixelised/HitLinearityPlot_20240821.png");
 
     return;
 }
--- GetEnergyResolution/scripts/runGetEnergyResolutionProcessor.sh
+++ GetEnergyResolution/scripts/runGetEnergyResolutionProcessor.sh
@@ -8,12 +8,12 @@ export MARLIN_DLL="$MARLIN_DLL:$PWD/../lib/libGetEnergyResolutionProcessor.so"
 
 declare -i numberEvents=10001
 
-for energy in 1500 3500 5500 7500 9500 11500 13500 15000
+for energy in 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000 6500 7000 7500 8000 8500 9000 9500 10000 10500 11000 11500 12000 12500 13000 13500 14000 14500 15000
 do
-    AIDAFilePath="/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/"
+    AIDAFilePath="/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/EnergyResolution/20240821_pixelised/"
     AIDAFileName="ECALe_luxe_v0_QGSP_BERT_e-_${energy}MeV_0"
 
-    InputLCIOFilePath="/lustre/ific.uv.es/prj/gl/abehep.flc/LUXE/ECALe_SimAnalysis/electron/PixelizationLCIO/"
-    InputLCIOFileName="new_ECALe_luxe_v0_QGSP_BERT_e-_${energy}MeV_0.slcio"
+    InputLCIOFilePath="/lhome/ific/a/almanzam/gluon/LUXE/ddsim/electron/pixelised/"
+    InputLCIOFileName="pixelised_ECALe_luxe_v0_QGSP_BERT_e-_${energy}MeV_0.slcio"
     Marlin --global.MaxRecordNumber=${numberEvents} --MyAIDAProcessor.FileName="$AIDAFilePath$AIDAFileName" --global.LCIOInputFiles=$InputLCIOFilePath$InputLCIOFileName test.xml
 done
